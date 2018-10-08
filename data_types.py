df23c3ed350ed6f59dff7b9b58aad2f6f3ab9888
Kayla Baum <kamabaum@ucsc.edu>
2018-10-07 22:37:16 -0700

    Update data_types.py

--- data_types.py
+++ data_types.py
@@ -15,7 +15,7 @@ x = 119.0		#floating point number
 print(type(x))	#print out the data type of x
 
 y = 1.19e2		#float 119 in sci notation
-print(type(y))	$#print out the data type of y
+print(type(y))	#print out the data type of y
 
 z = np.zeros(i,dtype=float)	#declare array of floats
 print(type(z))				#will return nd array
